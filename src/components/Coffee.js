80e5666bb01347cd2e12c0fc44ea8102b649ea23
ambi hidalgo <ambio.pk@gmail.com>
2023-06-03 19:38:10 -0700

    WIP handle sell

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -3,15 +3,20 @@ import PropTypes from "prop-types";
 import { v4 } from 'uuid';
 
 function Coffee(props){
+  const [weight, setWeight] = useState(props.weight);
+  const handleCoffeeSold = () => {
+    setWeight(prevWeight => prevWeight -1);
+  }
   return(
     <>
     <div onClick = {() => props.whenTicketClicked(props.id)}></div>
     <h3>{props.name} - {props.roast} </h3>
     <p><em>{props.origin}</em></p>
     <p>{props.price}</p>
-    <p>{props.weight}lb</p>
+    <p>{weight}lb</p>
+    <button onClick = {handleCoffeeSold}> Sell </button>
     </>
-  )
+  );
 }
 
 Coffee.propTypes = {
@@ -22,5 +27,7 @@ Coffee.propTypes = {
   id: PropTypes.string,
   weight: PropTypes.number
 }
-
+Coffee.defaultProps = {
+  weight: 130
+}
 export default Coffee;
